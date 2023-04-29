ae97381f33275cecf4f2428173a47322d0883868
Anas Bensaid <bensaidanass9@gmail.com>
2023-04-29 01:35:26 +0100

    deleting the github directory

---
+++ app/sections/Projects.tsx
@@ -0,0 +1,14 @@
+import AnimatedText from "../components/AnimatedText"
+import Container from "../components/Container"
+
+const Projects = () => {
+    return (
+        <Container>
+            <section className="w-full min-h-screen">
+                <AnimatedText text="Projects" className="mb-16" />
+            </section>
+        </Container>
+    )
+}
+
+export default Projects
